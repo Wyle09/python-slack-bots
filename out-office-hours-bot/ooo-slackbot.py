5de772872ad213233dafeca9a70a45f713bc78c2
wyle09 <wylecordero@gmail.com>
2021-10-16 16:15:12 -0400

    Adding slack post messaging

--- out-office-hours-bot/ooo-slackbot.py
+++ out-office-hours-bot/ooo-slackbot.py
@@ -1,7 +1,25 @@
+from calendar import SATURDAY
 import os
 from dotenv import load_dotenv, find_dotenv
+import slack
+
+
+# Reply to new messages
+# Reply to new messages within specific time windows
+
 
 load_dotenv(find_dotenv())
+client = slack.WebClient(token=os.getenv('SLACK_BOT_USER_OAUTH_TOKEN'))
 
-print(os.getenv('SLACK_BOT_USER_OAUTH_TOKEN'))
-print(os.getenv("OBSIDIAN"))
+
+def message(*channels):
+    """Post message to a list of channels"""
+    for c in channels:
+        client.chat_postMessage(channel=c, text='Testing bot message')
+
+def main():
+    message(*['#general', '#random'])
+
+
+if __name__ == '__main__':
+    main()
